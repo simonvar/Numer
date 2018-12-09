65042feec77489410f59cb265bd8ab70540516c1
simonvar <semavar3@gmail.com>
2018-12-09 21:46:37 +0300

    add bottom navigation

---
+++ app/src/main/java/com/shine/semav/numbersystem/BottomNavigationDrawerFragment.kt
@@ -0,0 +1,15 @@
+package com.shine.semav.numbersystem
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class BottomNavigationDrawerFragment: BottomSheetDialogFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_navigation, container, false)
+    }
+
+}
--- app/src/main/java/com/shine/semav/numbersystem/MainActivity.kt
+++ app/src/main/java/com/shine/semav/numbersystem/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.shine.semav.numbersystem
 
+import android.content.Context
 import android.os.Bundle
+import android.view.Gravity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         changeFragment(ConverterFragment(), getString(R.string.menu_title_converter))
-        bottom_bar.replaceMenu(R.menu.options)
+        setSupportActionBar(bottom_bar)
     }
 
     fun changeFragment(f: Fragment, title: String, cleanStack: Boolean = true) {
@@ -30,6 +36,25 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.title = title
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.options, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.options_info -> toast("Info")
+            R.id.options_settings -> toast("Settings")
+            android.R.id.home -> {
+                val bottomNavDrawerFragment = BottomNavigationDrawerFragment()
+                bottomNavDrawerFragment.show(supportFragmentManager, bottomNavDrawerFragment.tag)
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+        return true
+    }
+
     fun clearBackStack() {
         val manager = supportFragmentManager
         if (manager.backStackEntryCount > 0) {
@@ -41,9 +66,15 @@ class MainActivity : AppCompatActivity() {
     override fun onBackPressed() {
         val fragmentManager = supportFragmentManager;
         if (fragmentManager.backStackEntryCount > 1) {
-            fragmentManager.popBackStack();
+            fragmentManager.popBackStack()
         } else {
-            finish();
+            finish()
         }
+    }
+
+    fun Context.toast(message: CharSequence) {
+        val toast = Toast.makeText(this, message, Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.BOTTOM, 0, 325)
+        toast.show()
     }
 }
